64e8d2d5b549bf088b983f21ef258f082f831bfa
AlexCPT-cpu <67722561+AlexCPT-cpu@users.noreply.github.com>
2023-01-22 00:01:38 +0100

    Delete config directory

--- config/config.ts
+++
@@ -1,6 +0,0 @@
-export const nftContract: string = '0xbdAbD36F4465CEc7576e6237B626f91CA651F2c2'
-export const tokenContract: string = '0xe88c1726F0547CD73bff8599dF6728866a751b8f'
-export const nftStaking: string = '0x93Ce6C3f204814105F04259Df75bd1D203E70Aa7'
-export const tokenStaking: string = '0x1a53658108E1254e5C4532f5B992cEA8F63ade7C'
-
-export const provider: string = 'https://eth-goerli.g.alchemy.com/v2/br-vL2X5K6ZxKDZ8N_U9sEnkIkba9Zw6'
